2f9ff9245096e97d6cfd921c992a11ba155baa0d
Bart Platak <norfavrell@gmail.com>
2017-12-02 14:22:45 +0000

    Refactored solution to Day1A and solved Day1B

--- src/adventofcode_17/day1.clj
+++ src/adventofcode_17/day1.clj
@@ -1,19 +1,43 @@
 (ns adventofcode-17.day1
-  (:require [clojure.java.io :as io] [clojure.core.match :refer [match]]))
+  (:require [clojure.java.io :as io]
+            [clojure.core.match :refer [match]]
+            [clojure.test :refer :all]))
+(:use 'clojure.test)
 
-(defn solve [raw-data]
-  (defn solve' [dta]
-    (loop [data dta
-           res 0]
+; -- Rotate Right
+(with-test
+  (defn rot-right [data n]
+     (take (count data) (drop n (cycle data))))
 
-      (match [data]
-             [([x y] :seq)] (if (= x y) (+ res x) res)
-             [([x y & rest] :seq)] (recur (cons y rest) (if (= x y) (+ res x) res)))))
+  (is (= [1 2 2 1] (rot-right [1 1 2 2] 1)))
+  (is (= [2 2 1 1] (rot-right [1 1 2 2] 2))) )
 
-    (solve' (conj raw-data (last raw-data))))
+
+; -- Solve the problem
+(with-test
+  (defn solve [data n]
+    (reduce + (map (fn [x] (if (= (first x) (second x)) (first x) 0))
+                   (map vector data (rot-right data n)))) )
+
+  (is (= 3 (solve [1 1 2 2] 3)))
+  (is (= 4 (solve [1 1 1 1] 1)))
+  (is (= 0 (solve [1 2 3 4] 1)))
+  (is (= 9 (solve [9 1 2 1 2 1 2 9] 1)))
+  (is (= 6 (solve [1 2 1 2] 2)))
+  (is (= 0 (solve [1 2 2 1] 2)))
+  (is (= 4 (solve [1 2 3 4 2 5] 3)))
+  (is (= 12 (solve [1 2 3 1 2 3] 3)))
+  (is (= 4 (solve [1 2 1 3 1 4 1 5] 4))) )
+
+
+
 
 
 
 (defn -main [data-file]
-  (let [raw-data (map (fn [x] (Character/digit x 10)) (seq (slurp data-file)))]
-    (println (solve raw-data))))
+  (test #'rot-right)
+  (test #'solve)
+
+  (let [data (map (fn [x] (Character/digit x 10)) (seq (slurp data-file)))]
+    (println "PartA: " (solve data 1))
+    (println "PartB: " (solve data (/ (count data) 2)))))
--- test/adventofcode_17/day1_test.clj
+++ test/adventofcode_17/day1_test.clj
@@ -1,8 +1,8 @@
-(ns adventofcode-17.day1-test
-  (:require [clojure.test :refer :all]
-            [adventofcode-17.day1 :refer :all]))
-
-(deftest day1-test
-  (testing "Example 1"
-    (is (= 3 (solve [1 1 2 2])))))
-
+;(ns adventofcode-17.day1-test
+;  (:require [clojure.test :refer :all]
+;            [adventofcode-17.day1 :refer :all]))
+;
+;(deftest day1-test
+;  (testing "Example 1"
+;    (is (= 3 (solve [1 1 2 2])))))
+;
